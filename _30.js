09460dfed281e8a1dc735c6e479875a73bc2f250
Nathan V Garrett <ngarrett768@gmail.com>
2023-09-13 14:12:36 -0300

    @nathangarret adicionando testes de lógica em JS

--- _30.js
+++ _30.js
@@ -2,11 +2,11 @@ const entrada = require('prompt-sync')({ sigint: true })
 
 // Ler 3 valores (considere que não serão informados valores iguais) e escrevê-los em ordem crescente. 
 
-let numero_um = parseInt(entrada("Informe o primeiro valor: "));
+const numero_um = parseInt(entrada("Informe o primeiro valor: "));
 
-let numero_dois = parseInt(entrada("Informe o segundo valor: "));
+const numero_dois = parseInt(entrada("Informe o segundo valor: "));
 
-let numero_tres = parseInt(entrada("Informe o terceiro valor: "));
+const numero_tres = parseInt(entrada("Informe o terceiro valor: "));
 
 const valores = [
     numero_um,
